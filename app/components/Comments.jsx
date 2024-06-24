757c8556a9fa8da426dd813cb8a84631f7eb6b9b
Hallcoder <hallcoder25@gmail.com>
2024-06-24 13:38:47 +0200

    [feat]: Done

--- app/components/Comments.jsx
+++ app/components/Comments.jsx
@@ -1,40 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, ScrollView, Text, View } from 'react-native';
 import axios from 'axios';
 import { useRouter } from 'expo-router';
 import Toast from 'react-native-toast-message';
 import CommentCard from './CommentCard';
- 
+import CardComponent from './CardComponent';
+import { SafeAreaView } from 'react-native-safe-area-context';
 const Comments = ({comments}) => {
-  const [user,setUser] = useState({name:""});
-  const router = useRouter();
+  const [post,setPost] = useState({postId:""});
+  console.log("Comment at 0",comments[0])
 const [loading,setLoading] = useState(false);
+const [deleting,setDeleting] = useState(false);
+
+ useEffect(()=>{
+  setLoading(true);
+  if (comments.length > 0 && comments[0].postId) {
+    axios.get(`${process.env.EXPO_PUBLIC_API_URL}/posts/${comments[0].postId}`).then(d =>{
+      setPost(d.data);
+    }).catch(err =>{
+      Toast.show({
+        type:'error',
+        text1:"Something went wrong!",
+        text2:err.message
+      })
+    }).finally(()=>{
+    setLoading(false);
+    })
+  } else {
+   Toast.show({
+    type:"error",
+    text1:"Something went wrong!"
+   })
+  }
+ },[comments]);
+
   const deletePost = ()=>{
-    axios.delete(`${process.env.EXPO_PUBLIC_API_URL}/delete/${comments[0].postId}`).then(()=>{
+    setDeleting(true)
+    axios.delete(`${process.env.EXPO_PUBLIC_API_URL}/posts/${comments[0].postId}`).then((d)=>{
         Toast.show({
             type:'success',
             text1:"Deleted successfully!"
-        })
+        });
+        console.log(d);
     }).catch(err =>{
         Toast.show({
             type:'error',
             text1:'Something went wrong!',
             text2:err.message
-        })
+        });
+    }).finally(() =>{
+      setDeleting(false);
     })
   }
-  const goToPostDetails = () =>{
-router.push(`/post-details/${post.id}`);
-  }
-    {return !loading ? <View className="p-2 cursor-pointer border shadow-md border-gray-200">
+
+    {return !loading ? <SafeAreaView>
+     <ScrollView className="p-2 cursor-pointer shadow-md border-gray-200">
+      <Toast />
+      <Text className="text-2xl mb-2">Post {post.postId}</Text>
+      <CardComponent post={post} commentPage/>
       <Text className="font-semibold text-sm mt-4">Comments</Text>
      {comments.map(c =>{
-        return <CommentCard comment={c}/>
+        return <CommentCard comment={c} key={c.id}/>
      })}
-     <Pressable onPress={deletePost} className="p-2 w-11/12 rounded-md bg-red-600 ">
-        <Text className="text-white">Delete</Text>
+     <Pressable onPressIn={deletePost} className="p-2 mb-10 mx-auto w-11/12 rounded-md bg-red-600 ">
+        <Text className="text-white text-center">{deleting ? 'Deleting...':"Delete"}</Text>
      </Pressable>
-    </View>:<Text>user...</Text>}
+    </ScrollView></SafeAreaView>:<Text>user...</Text>}
 };
 
 
